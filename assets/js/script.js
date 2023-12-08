7d799500f71cc749a4cbea679ec7c03dc4accc4d
Adam McMains <adammcmains@gmail.com>
2023-12-07 21:58:31 -0800

    added restaurant API

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer sW-dsNBeUuGD3tQgKOFBeUzIj5oj8T_O96mWZEngST-4EB7JqJZ_UHOmk7VGf4QXiSeJvOPF0lPaIzapvTEHpd4oykiivjD860xqNlLzt4ndWkxnwGNkEA2z-KNyZXYx'
+    }
+  };
+  
+  fetch( 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=20', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
